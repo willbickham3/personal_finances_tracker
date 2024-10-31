30ba0a9d9ed862825dab005f62cc119dc7152bcf
Will <willbickham3@gmail.com>
2024-10-31 14:53:07 -0500

    Add 5000 as alternate port

--- user-service/app.js
+++ user-service/app.js
@@ -8,7 +8,7 @@ app.use(express.json()); // Middleware to parse JSON
 // Register the user routes under /api/users
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`User service running on port ${PORT}`);
 });
