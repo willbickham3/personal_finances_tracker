aca64627d316e10df8b6258598172aeb072ad6ba
Will <willbickham3@gmail.com>
2024-11-01 11:12:46 -0500

    Add token authorization system

--- user-service/userController.js
+++ user-service/userController.js
@@ -1,10 +1,13 @@
 const bcrypt = require('bcrypt')
 const pool = require('./db')
+const jwt = require('jsonwebtoken')
+
+const key = process.env.SECRET
 
 const saltRounds = 10;
 
 exports.createUser = async (req, res) => {
-    const { password, email } = req.body;
+    const { email, password } = req.body;
     try {
         const hash = await bcrypt.hash(password, saltRounds);
 
@@ -21,5 +24,28 @@ exports.createUser = async (req, res) => {
 }
 
 exports.userLogin = async (req, res) => {
-    return
+    const { loginPassword, email } = req.body;
+    try {
+        const result = await pool.query('SELECT user_id, password FROM users WHERE email = $1', [email]);
+
+        const user_id = result.rows[0].user_id;
+        const hashedPassword = result.rows[0].password;
+        console.log('ID: ', user_id)
+        console.log('hashedPassword: ', hashedPassword)
+
+        const isMatch = await bcrypt.compare(loginPassword, hashedPassword);
+        if (isMatch) {
+            // generate token
+            const token = jwt.sign({ user_id }, key, { expiresIn: '1d'})
+            res.json({ message: 'Login Successful', token });
+        }
+        else {
+            return res.status(401).json({ error: 'Incorrect email or password' });
+            // err (user doesnt exist or password is correct)
+        }
+    }
+    catch (error) {
+        console.error('Error during login: ', error);
+        return res.status(500).json({ error: 'An error occured during login' });
+    }
 }
