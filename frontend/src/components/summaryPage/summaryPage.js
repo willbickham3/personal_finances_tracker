d33dae3f0e2f021d46add47df06bbedeffc106ff
Will <willbickham3@gmail.com>
2024-11-02 23:31:22 -0500

    Add basic nav bar to other pages

--- frontend/src/components/summaryPage/summaryPage.js
+++ frontend/src/components/summaryPage/summaryPage.js
@@ -1,9 +1,24 @@
 import { React} from 'react';
+import { Link } from 'react-router-dom';
+import './summaryPage.css'
 
 const SummaryPage = () => {
     return (
         <>
-        <div>Hello dere.</div>
+        <nav className="navbar">
+      <div className="navbar-container">
+        <div className='nav-item logo'>Personal Finances Tracker</div>
+        <div className='nav-item'><Link to="/income" className="nav-link">
+              Incomes
+            </Link></div>
+        <div className='nav-item'><Link to="/expenses" className="nav-link">
+              Expenses
+            </Link></div>
+        <div className='nav-item'><Link to="/budgets" className="nav-link">
+              Budgets
+            </Link></div>
+      </div>
+    </nav>
         </>
     )
 }
