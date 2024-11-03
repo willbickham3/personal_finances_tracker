92c20008da6a0d803b47b900b4b21bc14ca13bbc
Will <willbickham3@gmail.com>
2024-11-02 23:30:05 -0500

    Income boilerplate

---
+++ frontend/src/components/incomePage/Income.js
@@ -0,0 +1,13 @@
+import { React, useState} from 'react';
+
+const IncomePage = () => {
+    const [ income, setIncome ] = useState('')
+
+    return (
+        <>
+        <div>Income</div>
+        </>
+    )
+}
+
+export default IncomePage
