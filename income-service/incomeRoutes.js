b901d1ec652112e5bb5a5eb99abdadf4be650e54
Will <willbickham3@gmail.com>
2024-11-04 13:09:36 -0600

    remove route parameter because its being passed in req.body

--- income-service/incomeRoutes.js
+++ income-service/incomeRoutes.js
@@ -5,7 +5,7 @@ const incomeController = require('./incomeController');
 // Retrieve all the incomes for a user
 router.get('/:user_id', incomeController.getAllIncome);
 router.post('/', incomeController.addIncome)
-router.put('/:id', incomeController.updateIncome)
+router.put('/', incomeController.updateIncome)
 router.delete('/:user_id/:id', incomeController.deleteIncome)
 
 module.exports = router
