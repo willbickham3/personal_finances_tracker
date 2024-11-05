6683f990162b70016b110c1b28cd17539679b4a0
Will <willbickham3@gmail.com>
2024-11-05 03:57:26 -0600

    Update with NavBar component

--- frontend/src/components/summaryPage/summaryPage.js
+++ frontend/src/components/summaryPage/summaryPage.js
@@ -1,25 +1,12 @@
 import { React} from 'react';
 import { Link } from 'react-router-dom';
 import './summaryPage.css'
+import NavBar from '../utils/NavBar';
 
 const SummaryPage = () => {
     return (
         <>
-        <nav className="navbar">
-      <div className="navbar-container">
-        <div className='nav-item logo'>Personal Finances Tracker</div>
-        <div className='nav-item'><Link to="/income" className="nav-link">
-              Incomes
-            </Link></div>
-        <div className='nav-item'><Link to="/expenses" className="nav-link">
-              Expenses
-            </Link></div>
-        <div className='nav-item'><Link to="/budgets" className="nav-link">
-              Budgets
-            </Link></div>
-      </div>
-      <GetUserId/>
-    </nav>
+        <NavBar />
         </>
     )
 }
