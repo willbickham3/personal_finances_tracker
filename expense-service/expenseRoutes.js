2ef5257162c23c02c2b093928b21e024c400f025
Will <willbickham3@gmail.com>
2024-11-05 03:55:46 -0600

    Remove parameter from route

--- expense-service/expenseRoutes.js
+++ expense-service/expenseRoutes.js
@@ -5,7 +5,7 @@ const expenseController = require('./expenseController');
 // Retrieve all the expenses for a user
 router.get('/:user_id', expenseController.getAllExpenses);
 router.post('/', expenseController.addExpense)
-router.put('/:id', expenseController.updateExpense)
+router.put('/', expenseController.updateExpense)
 router.delete('/:user_id/:id', expenseController.deleteExpense)
 
 module.exports = router
