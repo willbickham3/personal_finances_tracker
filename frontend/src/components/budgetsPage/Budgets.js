005b782b9613fbe60ee9e5f68eaebdafad237754
Will <willbickham3@gmail.com>
2024-11-05 03:58:49 -0600

    Begin Budgets page

--- frontend/src/components/budgetsPage/Budgets.js
+++ frontend/src/components/budgetsPage/Budgets.js
@@ -1,10 +1,16 @@
 import { React, useState} from 'react';
-
+import ProgressBar from 'react-bootstrap/ProgressBar'
 import NavBar from '../utils/NavBar';
 
 const BudgetPage = () => {
     const [ budget, setBudget ] = useState('')
-
+    const [ currentAmount, setCurrentAmount ] = useState('')
+    const [ isModalOpen, setIsModalOpen ] = useState(false)
+    const [ isEditing, setEditing ] = useState(false)
+    const [ amount,   setAmount ]   = useState('')
+    const [ category, setCategory ] = useState('')
+    const [ date,     setDate ]     = useState('')
+    const [ itemId, setItemId ] = useState('')
     return (
         <>
         <NavBar />
