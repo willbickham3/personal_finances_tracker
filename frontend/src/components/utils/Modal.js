a3252299c048d377f63a68925c8a07cbc0e882d0
Will <willbickham3@gmail.com>
2024-11-06 01:24:36 -0600

    Add children prop

--- frontend/src/components/utils/Modal.js
+++ frontend/src/components/utils/Modal.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const PopupModal = ({ isOpen, onClose, title }) => {
+const PopupModal = ({ isOpen, onClose, title, children }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="modal-overlay" onClick={onClose}>
+        <div className="modal-overlay" >
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <h2>{title}</h2>
                 <button className="close-button" onClick={onClose}>X</button>
             </div>
+            {children}
         </div>
     );
 };
