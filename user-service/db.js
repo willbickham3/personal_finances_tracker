8cdbb923baa97d9d3d8093ee46553c78e9d3d6f3
Will <willbickham3@gmail.com>
2024-11-24 11:59:11 -0600

    Add ssl for render deployment

--- user-service/db.js
+++ user-service/db.js
@@ -6,7 +6,8 @@ const pool = new Pool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
