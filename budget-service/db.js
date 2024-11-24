4f4adda87575bb70817c352159ca930bd7211f0a
Will <willbickham3@gmail.com>
2024-11-24 12:44:08 -0600

    Add SSL for render

--- budget-service/db.js
+++ budget-service/db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
