04a47468ccfe82cae4bd631c12c39b436ef9d13b
Will <willbickham3@gmail.com>
2024-12-02 00:39:08 -0600

    Forgot comma

--- user-service/db.js
+++ user-service/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
     // host: process.env.DB_HOST,
     // port: process.env.DB_PORT,
     // user: process.env.DB_USER,
