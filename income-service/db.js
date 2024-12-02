2137ec0c698eafdde68a5b94d33070f1115830f9
Will <willbickham3@gmail.com>
2024-12-02 00:46:41 -0600

    Update other services DB urls

--- income-service/db.js
+++ income-service/db.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
     ssl: { rejectUnauthorized: false }
 });
 
