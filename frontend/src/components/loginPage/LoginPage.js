f30ccae52b69c5155557454796c610eb2194fb9b
Will <willbickham3@gmail.com>
2024-12-17 14:04:25 -0600

    Add deployment route to POST request

--- frontend/src/components/loginPage/LoginPage.js
+++ frontend/src/components/loginPage/LoginPage.js
@@ -73,7 +73,7 @@ const LoginPage = () => {
 
     const handleSignUpClick = async () => {
         if (page ==="Sign Up") {
-            const response = await fetch('http://localhost:5005/validate-email', {
+            const response = await fetch('https://b-emailvalidation.onrender.com', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
