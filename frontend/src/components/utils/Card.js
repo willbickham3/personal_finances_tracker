0d82540c516a2ebb725e85e22ad2739bbc0c2ce1
Will <willbickham3@gmail.com>
2024-12-20 13:45:55 -0600

    Create: Card logic for creating budget cards

--- frontend/src/components/utils/Card.js
+++ frontend/src/components/utils/Card.js
@@ -1,14 +1,50 @@
-export const card = (category, amount, limit) => {
-    // Create card-container element and give it a class
-    let cardContainer = document.createElement('article');
-    cardContainer.classList.add('card-container');
+export const Card = ({ budget }) => {
+    // Takes in a budget entry and creates a card with its values
+    return (
+    <>
+        <div className="budget-card">
+          <h4>{budget.category}</h4>
+          <p>
+            {budget.current_amount} / <b>{budget.amount}</b>
+          </p>
+          <ProgressBar currentAmount={budget.current_amount} totalAmount={budget.amount}/>
+        </div>
+    </>
+      )
+}
 
-    // 
-    let categoryContainer = document.createElement('header');
-    let amountContainer = document.createElement('p');
+const ProgressBar = ({ currentAmount, totalAmount}) => {
+    const progress = ((Number(currentAmount) / Number(totalAmount)) * 100)
+    let barColor = chooseBarColor(progress)
 
-    categoryContainer.innerHTML = `<span>${category}</span>`
-    amountContainer.innerHTML = `<span>$${amount}</span> / $${limit}`
-    cardContainer.append(categoryContainer, amountContainer)
-    return cardContainer
+    return (
+      <div className='progress-bar-container' style={{ width: '100%', backgroundColor:'white', borderRadius: '10px'}}>
+        <div className='progress-bar'
+          style={{
+            width: `${progress}%`,
+            height: '100%',
+            backgroundColor: `${barColor}`,
+            borderRadius: '10px',
+            transition: 'width 0.3s ease',
+          }}>
+        </div>
+      </div>
+    )
+  }
+
+function chooseBarColor(progress) {
+    let bgColor;
+    if (progress > 100) {
+      bgColor = 'red'
+    }
+    else if (progress > 80) {
+      bgColor = 'orange'
+    }
+    else if (progress > 50) {
+      bgColor = 'yellow'
+    }
+    else {
+      bgColor = 'green'
+    }
+    return bgColor
 }
